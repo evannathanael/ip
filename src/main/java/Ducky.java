926c2ee952133f1efd7f7ecf8ea57f468c955d98
Evan Nathanael <evan.nathanael07@gmail.com>
2026-08-19 23:04:58 +0800

    Rename chatbot to Ducky and updated the banner

---
+++ src/main/java/Ducky.java
@@ -0,0 +1,10 @@
+public class Ducky {
+    public static void main(String[] args) {
+        String banner = " ____  _   _  ____ _  ____   __\n"
+                + "|  _ \\| | | |/ ___| |/ /\\ \\ / /\n"
+                + "| | | | | | | |   | ' /  \\ V / \n"
+                + "| |_| | |_| | |___| . \\   | |  \n"
+                + "|____/ \\___/ \\____|_|\\_\\  |_|  \n";
+        System.out.println(banner);
+    }
+}
