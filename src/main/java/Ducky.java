77d1a9c56caa71f506415a61745405e4a144ec47
Evan Nathanael <evan.nathanael07@gmail.com>
2026-08-20 00:33:37 +0800

    Add in-memory task storage and list command

--- src/main/java/Ducky.java
+++ src/main/java/Ducky.java
@@ -1,24 +1,55 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
 /**
  * Represents the main chatbot application.
  */
 public class Ducky {
-    public static String LINE = "____________________________________________________________";
-    public static String BANNER = " ____  _   _  ____ _  ____   __\n"
+    private static final String LINE = "____________________________________________________________";
+    private static final String BANNER = " ____  _   _  ____ _  ____   __\n"
             + "|  _ \\| | | |/ ___| |/ /\\ \\ / /\n"
             + "| | | | | | | |   | ' /  \\ V / \n"
             + "| |_| | |_| | |___| . \\   | |  \n"
             + "|____/ \\___/ \\____|_|\\_\\  |_|  \n";
+    private static final List<String> tasks = new ArrayList<>();
+
+    /**
+     * Adds a task to the existing tasks
+     */
+    public static void addTask(String msg) {
+        tasks.add(msg);
+    }
 
     /**
      * Prints the chatbot's response surrounded by separator lines.
      */
     public static void printEchoMessage(String msg) {
         System.out.println(LINE);
-        System.out.println(msg);
+        System.out.println("added: " + msg);
         System.out.println(LINE);
     }
+
+    /**
+     * Prints the chatbot's exit response.
+     */
+    public static void printExitMessage() {
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+
+    /**
+     * Prints all task in tasks
+     */
+    public static void printTasks() {
+        System.out.println(LINE);
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(String.format("%d. %s", i + 1, tasks.get(i)));
+        }
+        System.out.println(LINE);
+    }
+
     public static void main(String[] args) {
         System.out.println(LINE);
         System.out.println(BANNER);
@@ -31,11 +62,13 @@ public class Ducky {
             String msg = scanner.nextLine();
             if (msg.equals("bye")) {
                 break;
+            } else if (msg.equals("list")) {
+                printTasks();
+            } else {
+                addTask(msg);
+                printEchoMessage(msg);
             }
-            printEchoMessage(msg);
         }
-        System.out.println(LINE);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(LINE);
+        printExitMessage();
     }
 }
