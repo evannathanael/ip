6abe110dd55cebefd422e8fb3fca5646611ab6ba
Evan Nathanael <evan.nathanael07@gmail.com>
2026-08-19 23:32:47 +0800

    Updated chatbot to greet the user and exit

--- src/main/java/Ducky.java
+++ src/main/java/Ducky.java
@@ -1,10 +1,17 @@
 public class Ducky {
     public static void main(String[] args) {
+        String line = "____________________________________________________________";
         String banner = " ____  _   _  ____ _  ____   __\n"
                 + "|  _ \\| | | |/ ___| |/ /\\ \\ / /\n"
                 + "| | | | | | | |   | ' /  \\ V / \n"
                 + "| |_| | |_| | |___| . \\   | |  \n"
                 + "|____/ \\___/ \\____|_|\\_\\  |_|  \n";
+        System.out.println(line);
         System.out.println(banner);
+        System.out.println("Hello! I'm Ducky.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
